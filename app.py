d31e7312a3792cd1ca5bacef82cfb472cab7dd71
kdmgs110 <kdmgs110@gmail.com>
2017-12-09 14:57:36 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging, Response
+import sys
+import os
+from selenium import webdriver
+import pandas
+import time
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("home.html")
+
+@app.route("/", methods=["POST"])
+def post():
+    query = request.form["query"]
+    csv = exportCSV(query)
+    with open("output.csv") as fp:
+            csv = fp.read()
+            return Response(
+                csv,
+                mimetype="text/csv",
+                headers={"Content-disposition":
+                         "attachment; filename=output.csv"})
+
+
+def exportCSV(query):
+	#Googleにアクセスして、タイトルをとってこれるかテスト
+	browser = webdriver.Chrome(executable_path='/mnt/c/workspace/pydev/chromedriver.exe')
+	df = pandas.read_csv('default.csv', index_col=0)
+	query = query
+	browser.get("https://www.mercari.com/jp/search/?sort_order=price_desc&keyword={}&category_root=&brand_name=&brand_id=&size_group=&price_min=&price_max=".format(query))
+	page = 1
+	while True: #continue until getting the last page
+	    if len(browser.find_elements_by_css_selector("li.pager-next .pager-cell:nth-child(1) a")) > 0:
+	        print("######################page: {} ########################".format(page))
+	        print("Starting to get posts...")
+	        posts = browser.find_elements_by_css_selector(".items-box")
+	        for post in posts:
+	            title = post.find_element_by_css_selector("h3.items-box-name").text
+	            price = post.find_element_by_css_selector(".items-box-price").text
+	            price = price.replace('¥', '')
+	            sold = 0
+	            if len(post.find_elements_by_css_selector(".item-sold-out-badge")) > 0:
+		            sold = 1
+	            url = post.find_element_by_css_selector("a").get_attribute("href")
+	            se = pandas.Series([title, price, sold,url],['title','price','sold','url'])
+	            df = df.append(se, ignore_index=True)
+	        page+=1
+	        btn = browser.find_element_by_css_selector("li.pager-next .pager-cell:nth-child(1) a").get_attribute("href")
+	        print("next url:{}".format(btn))
+	        browser.get(btn)
+	        print("Moving to next page......")
+	    else:
+	        print("no pager exist anymore")
+	        break
+	df.to_csv("output.csv")
+
+if __name__ == '__main__':
+    app.run(debug=True) # デバックしたときに、再ロードしなくても大丈夫になる
---
+++ tinderAuth.py
@@ -0,0 +1,47 @@
+# encoding: utf8                                                                                                                                           1,1           Top# encoding: utf8
+import argparse
+from datetime import datetime
+import json
+from random import randint
+import requests
+import sys
+from time import sleep
+import robobrowser
+import re
+
+headers = {
+    'app_version': '3',
+    'platform': 'ios',
+}
+
+def get_access_token(email, password):
+    MOBILE_USER_AGENT = r"Mozilla/5.0 (Linux; U; en-gb; KFTHWI Build/JDQ39) AppleWebKit/535.19 (KHTML, like Gecko) Silk/3.16 Safari/535.19"
+    FB_AUTH = "https://m.facebook.com/v2.6/dialog/oauth?redirect_uri=fb464891386855067%3A%2F%2Fauthorize%2F&display=touch&state=%7B%22challenge%22%3A%22IUUkEUqIGud332lfu%252BMJhxL4Wlc%253D%22%2C%220_auth_logger_id%22%3A%2230F06532-A1B9-4B10-BB28-B29956C71AB1%22%2C%22com.facebook.sdk_client_state%22%3Atrue%2C%223_method%22%3A%22sfvc_auth%22%7D&scope=user_birthday%2Cuser_photos%2Cuser_education_history%2Cemail%2Cuser_relationship_details%2Cuser_friends%2Cuser_work_history%2Cuser_likes&response_type=token%2Csigned_request&default_audience=friends&return_scopes=true&auth_type=rerequest&client_id=464891386855067&ret=login&sdk=ios&logger_id=30F06532-A1B9-4B10-BB28-B29956C71AB1&ext=1470840777&hash=AeZqkIcf-NEW6vBd"
+    s = robobrowser.RoboBrowser(user_agent=MOBILE_USER_AGENT, parser="lxml")
+    s.open(FB_AUTH)
+    f = s.get_form()
+    f["pass"] = password
+    f["email"] = email
+    s.submit_form(f)
+    f = s.get_form()
+    if f.submit_fields.get('__CONFIRM__'):
+        s.submit_form(f, submit=f.submit_fields['__CONFIRM__'])
+        print("[SUCCESS] got access token")
+    else:
+        raise Exception("Couldn't find the continue button. Maybe you supplied the wrong login credentials? Or maybe Facebook is asking a security question?")
+    access_token = re.search(r"access_token=([\w\d]+)", s.response.content.decode()).groups()[0]
+    return access_token
+
+def auth_token(fb_auth_token, fb_user_id):
+    h = headers
+    h.update({'content-type': 'application/json'})
+    req = requests.post(
+        'https://api.gotinder.com/auth',
+        headers=h,
+        data=json.dumps({'facebook_token': fb_auth_token, 'facebook_id': fb_user_id})
+    )
+    try:
+        return req.json()['token']
+        print("[SUCCESS] got auth_token")
+    except:
+        return None

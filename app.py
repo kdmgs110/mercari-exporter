97e4679ba6a9bf1fbedd7a9f935e01972700775b
kdmgs110 <kdmgs110@gmail.com>
2017-12-09 18:12:46 +0900

    fixed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect, url_for, session, request, logging, Response
+from flask import Flask, render_template, request, logging, Response
 import sys
 import os
 from selenium import webdriver
